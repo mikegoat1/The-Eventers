895536ee5fbbb0b780e614d99a3db9a15d07da93
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-09-20 05:33:35 -0700

    Add mongoose connection and user model
    - Added a new file `mongoose.jsx` to establish a connection to the MongoDB database using Mongoose.
    - Created a user model in the `user.jsx` file with fields for username, password, and createdAt. Reconfig register to use next.js instead of express

--- event_management_system/src/models/user.jsx
+++ event_management_system/src/models/user.jsx
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     createdAt: {type: Date, default: Date.now},
 });
--- event_management_system/src/pages/api/auth/register.jsx
+++ event_management_system/src/pages/api/auth/register.jsx
@@ -1,15 +1,32 @@
-const express = require('express');
-const router = express.Router();
+import bcrypt from 'bcrypt';
+import connetectToDatabase from '@/lib/mongoose';
+import User from '@/models/user';
 
-// POST /api/auth/register
-router.post('/register', (req, res) => {
+
+const  handler = async (req, res) => {
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
+
     const { username, password } = req.body;
 
-    if (username === 'admin' && password === 'password') {
-        res.status(200).json({ message: 'Register successful' });
-    } else {
-        res.status(401).json({ message: 'Invalid credentials' });
-    }
-});
+    try{
+        await connetectToDatabase();
 
-module.exports = router;
+        let user = await User.findOne({ username });
+        if(user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        user = new User({ username, password: hashedPassword });
+        await user.save();
+        res.status(200).json({ message: 'Register successful' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = handler;
