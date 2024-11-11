315dca1f87b8403e2bba7ee7c3d80b1a0759a528
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-11-11 09:26:15 -0800

    Refactor event handler to improve method handling and error management; update .gitignore and add VSCode launch configuration

--- event_management_system/src/pages/api/events/[id]/index.jsx
+++ event_management_system/src/pages/api/events/[id]/index.jsx
@@ -1,18 +1,24 @@
-import  { getEventById, updateEvent, deleteEvent } from '@/controllers/eventController';
+import {
+  getEventById,
+  updateEvent,
+  deleteEvent,
+} from "@/controllers/eventController";
 
-export default function handlerID( req, res) {
-    const { id } = req.query;
-    if( req.method === 'GET') {
-        return getEventById(req, res, id);
+export default function handlerID(req, res) {
+  const { id } = req.query;
+  try {
+    if (req.method === "GET") {
+      return getEventById(req, res, id);
+    } else if (req.method === "PUT") {
+      return updateEvent(req, res, id);
+    } else if (req.method === "DELETE") {
+      return deleteEvent(req, res, id);
+    } else {
+      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-    else if( req.method === 'PUT') {
-        return updateEvent(req, res, id);
-    }
-    else if( req.method === 'DELETE') {
-        return deleteEvent(req, res, id);
-    }
-    else {
-        res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
