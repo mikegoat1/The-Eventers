0ae0191c8ce33181c432c145ca4e98450b27a2ad
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2025-05-12 11:47:36 -0700

    chore: remove unused import statement in mongoose.js

--- event_management_system/src/lib/mongoose.js
+++ event_management_system/src/lib/mongoose.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 // import path from 'path';
 // import { fileURLToPath } from 'url';
-
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
