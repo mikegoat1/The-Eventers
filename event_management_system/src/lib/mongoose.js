8a7a324c491d8db2aea81feeeffe4d01da8dc291
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2025-05-12 12:06:20 -0700

    chore: remove unused imports and commented-out code in mongoose.js

--- event_management_system/src/lib/mongoose.js
+++ event_management_system/src/lib/mongoose.js
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
-// dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 dotenv.config();
 const MONGODB_URI =
   process.env.MONGODB_URI ||
